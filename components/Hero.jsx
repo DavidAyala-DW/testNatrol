9fd96ea4b8651ae36ddaf1a0efcba348447047d8
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-06-13 09:23:26 -0500

    remove logo in footer(temporaly)

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,51 +6,38 @@ import Button from './UI/Button';
 function Hero() {
   return (
 
-    <div className="w-full relative">
+    <div className="w-full relative h-max">
 
-      <div className="hidden xl:block">        
-
-        <Image
-          src="/hero_banner.png"
-          alt="alt"
-          layout='responsive'
-          width={1440}
-          height={925}
-          priority={true}
-          quality={100}
-        />
-
-      </div>
-
-      <div className="block xl:hidden h-[calc(100vw*1.3)] max-h-[800px] md:max-h-[700px] lg:max-h-[800px] overflow-hidden">        
+      <div className="absolute inset-0 w-full h-full z-[-1]">        
 
         <Image
           src="/hero_banner.png"
           alt="alt"
-          layout='responsive'
-          width={1440}
-          height={1900}
+          layout='fill'
           priority={true}
           quality={100}
+          className="object-cover"
         />
 
       </div>
 
-      <div className="absolute inset-0 w-full h-full bg-hero"></div>
+      <div className="absolute inset-0 w-full h-full bg-hero z-0"></div>
 
-      <div className="absolute inset-0 md:-top-5">
+      <div className="flex flex-col">
 
-        <div className="w-full flex flex-col h-full max-w-[95%] md:max-w-[62.5%] mx-auto">
+        <div className="flex flex-col h-full w-[95%] md:w-[62.5%] max-w-[900px] mx-auto  z-20 pb-10">
 
-          <Image
-            src="/hero_main.png"
-            alt="alt"
-            layout='responsive'
-            width={900}
-            height={688}
-            priority={true}
-            quality={100}
-          />
+          <div className="block -translate-y-5">
+            <Image
+              src="/hero_main.png"
+              alt="alt"
+              layout='responsive'
+              width={900}
+              height={688}
+              priority={true}
+              quality={100}
+            />
+          </div>
 
           <a
             className="w-max mb-4 md:mb-[30px] mx-auto "
