8642e2c155949767c3b62c2a0c7ab84560a72111
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-06-29 14:28:23 -0500

    test natrol part 3

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,7 +10,7 @@ export default function Document() {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(          { 
             "@context": "https://schema.org",
             "@type": "Corporation",
-            "name": "",
+            "name": "23131",
             "url": "https://natrol.myshopify.com",
             "logo": "https://cdn.shopify.com/s/files/1/0616/2130/5564/files/header-logo_2.svg?v=7058060604916427305",
             "sameAs": [
@@ -21,6 +21,42 @@ export default function Document() {
             "https://www.youtube.com/user/natrolinc/videos"
             ]
           }) }}
+        >
+        </script>
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(          
+            {
+             "@context": "https://schema.org/",
+             "@type": "Product",
+             "name": "Sleep+ Calm Capsules",
+             "image": "https://cdn.shopify.com/s/files/1/0616/2130/5564/products/Natrol-SleepPlus-Calm-Capsules-30ct-Front_320x.png?v=1654659896",
+             "description": "Natrol Sleep+ Calm helps calm an active mind and body, and ease you to sleep.†",
+             "brand": {
+             "@type": "Brand",
+             "name": "Natrol"
+             },
+             "sku": "7914",
+             "gtin12": "047469005948",
+             "offers": {
+             "@type": "Offer",
+             "url": "/products/sleep-plus-calm-sleep-support-capsules",
+             "priceCurrency": "USD",
+             
+             "price": "17",
+             "availability": "https://schema.org/InStock",
+             "itemCondition": "https://schema.org/NewCondition"
+             },
+             "aggregateRating": {
+             "@type": "AggregateRating",
+             "ratingValue": "4.1",
+             "bestRating": "5",
+             "worstRating": "1",
+             "ratingCount": "26"
+             }
+            }
+            ) }}
         >
         </script>
 
