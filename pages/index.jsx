8f5afe043a044010ee531a249379ebd1dc2e42da
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-06-29 15:48:16 -0500

    test natrol part 10

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,30 +23,32 @@ export default function Home() {
 
   useEffect(() => {
 
-    const head =  document.head;
-    const script = document.createElement("script");
-    script.innerHTML = `        {
-      "@context": "https://schema.org/", 
-      "@type": "BreadcrumbList", 
-      "itemListElement": [{
-        "@type": "ListItem", 
-        "position": 0, 
-        "name": "Home",
-        "item": "https://natrol.myshopify.com"  
-      },{
-        "@type": "ListItem", 
-        "position": 1, 
-        "name": "Adults",
-        "item": "https://natrol.myshopify.com/collections/adults-supplements"  
-      },{
-        "@type": "ListItem", 
-        "position": 2, 
-        "name": "Sleep+ Calm Capsules",
-        "item": "https://natrol.myshopify.com/products/sleep-plus-calm-sleep-support-capsules"  
-      }]
-    }`; 
-    script.type="application/ld+json";
-    head.appendChild(script);
+    setTimeout( () => {
+      const head =  document.head;
+      const script = document.createElement("script");
+      script.innerHTML = `        {
+        "@context": "https://schema.org/", 
+        "@type": "BreadcrumbList", 
+        "itemListElement": [{
+          "@type": "ListItem", 
+          "position": 0, 
+          "name": "Home",
+          "item": "https://natrol.myshopify.com"  
+        },{
+          "@type": "ListItem", 
+          "position": 1, 
+          "name": "Adults",
+          "item": "https://natrol.myshopify.com/collections/adults-supplements"  
+        },{
+          "@type": "ListItem", 
+          "position": 2, 
+          "name": "Sleep+ Calm Capsules",
+          "item": "https://natrol.myshopify.com/products/sleep-plus-calm-sleep-support-capsules"  
+        }]
+      }`; 
+      script.type="application/ld+json";
+      head.appendChild(script);
+    },400)
 
   }, []);
 
