1ae1569118be28b2614983fa3536f71d6a5f78bd
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-06-29 13:06:30 -0500

    test natrol part 1

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,23 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="shortcut icon" href="/favicon.webp" />        
+        
+        <script type="application/ld+json"> 
+      {
+{       "@context": "https://schema.org",
+       "@type": "Corporation",
+       "name": "",
+       "url": "https://natrol.myshopify.com",
+       "logo": "https://cdn.shopify.com/s/files/1/0616/2130/5564/files/header-logo_2.svg?v=7058060604916427305",
+       "sameAs": [
+       "https://www.facebook.com/natrol",
+       "https://twitter.com/natrol",
+       "https://www.instagram.com/natrolofficial/?hl=en",
+       "https://www.pinterest.com/natrolofficial/",
+       "https://www.youtube.com/user/natrolinc/videos"
+       ]}
+      }
+    </script>
       </Head>
       <body>
         <Main />
